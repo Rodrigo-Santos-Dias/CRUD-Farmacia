7a07ed9fa1d114900fc956ce3e40c5c500f6fde9
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-07-22 11:02:36 -0300

    Commit de configuração do Projeto

---
+++ src/test/java/com/Pharma/PharmaDelivery/PharmaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pharma.PharmaDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
